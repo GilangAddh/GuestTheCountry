d7b68ff5c2e3f10fb8326d6b352de74f36ce6cd5
GilangAddh <gilangadhiperkasa@gmail.com>
2024-06-06 10:45:14 +0700

    Template Guest Country

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+
 import HomeScreen from './src/screens/HomeScreen';
 import GuessTheCountryScreen from './src/screens/GuessTheCountryScreen';
+import MainNavigator from './src/navigator/MainNavigator';
+
+import WinScreen from './src/screens/WinScreen';
 const App = () => {
 return (
 <SafeAreaProvider>
-  <HomeScreen/>
+  <MainNavigator/>
 </SafeAreaProvider>
 )
 };
